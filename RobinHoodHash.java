4c6f32cb5dcfdaab08300fe328b87408ecf82832
Gribsy <mattmcormatheos04@gmail.com>
2024-11-27 18:30:01 +0200

    Update RobinHoodHash

--- RobinHoodHash.java
+++ RobinHoodHash.java
@@ -4,11 +4,26 @@ public class RobinHoodHash {
 	
 	char elements[];
 	int maxProbeLength;
+	int size;
+	int capacity;
 	
 
+	public RobinHoodHash() {
+		this(7);
+	}
+	
+	public RobinHoodHash(int capacity) {
+		
+		this.size=0;
+		this.capacity=capacity;
+		this.maxProbeLength=0;
+		
+		elements = new char[this.capacity];
+	}
+	
 	public static void main(String[] args) {
 		
 		
 	}
 
-}
+}
